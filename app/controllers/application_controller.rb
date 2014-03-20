ccf92dad5c6e840d8fcb0a5ad91e0b8632f725a8
Andrew <andrewhuth2@live.com>
2014-03-20 06:51:04 -0400

    Rescue from ParametersMissing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Rescue from ParametersMissing, which happens when a required parameter is
+  # an empty object {}.
+  rescue_from(ActionController::ParameterMissing) do |exception|
+    errors = ["#{exception}"]
+    render json: { errors: errors }, status: :unprocessable_entity
+  end
 end
