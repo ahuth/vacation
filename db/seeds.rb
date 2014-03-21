a70b5627d85ec2a76f2c47a51b25f68e407f3a79
Andrew <andrewhuth2@live.com>
2014-03-21 15:05:02 -0400

    Added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@ if Rails.env.development?
   User.create(email: "one@test.com", password: "secretsecret", password_confirmation: "secretsecret")
   Group.create(name: "Plating", user_id: 1)
   Group.create(name: "Heat Treat", user_id: 1)
+  Employee.create(name: "Andrew", hired: 1.day.ago.to_date, group_id: 1)
+  Request.create(date: Date.today.to_date, approved: true, employee_id: 1)
 end
