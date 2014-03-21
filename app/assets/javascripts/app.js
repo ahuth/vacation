327383b215c6b5c71b15fb2e504b75d5f7d62697
Andrew <andrewhuth2@live.com>
2014-03-20 23:17:18 -0400

    'Hello world' groups page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,7 @@
 /*jslint vars: true, browser: true , nomen: true, indent: 2*/
 /*global angular */
 
-angular.module("app", ["templates", "ngRoute", "ui.bootstrap"]);
+angular.module("pages", ["pages.groups"]);
+angular.module("pages.groups", []);
+
+angular.module("app", ["templates", "ngRoute", "ui.bootstrap", "pages"]);
---
+++ app/assets/javascripts/pages/groups/groups.js
@@ -0,0 +1,7 @@
+/*jslint vars: true, browser: true , nomen: true, indent: 2*/
+/*global angular */
+
+angular.module("pages.groups").controller("groups", ["$scope", function ($scope) {
+  "use strict";
+  $scope.hello = "world";
+}]);
