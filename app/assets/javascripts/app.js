48dbc9bfcef65305e1ca2f6b07ad4615dc9b06f5
Andrew <andrewhuth2@live.com>
2014-03-21 08:39:57 -0400

    Show list of groups

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,10 @@
 /*jslint vars: true, browser: true , nomen: true, indent: 2*/
 /*global angular */
 
+angular.module("services.data", []);
+angular.module("services.modal", []);
+
 angular.module("pages", ["pages.groups"]);
-angular.module("pages.groups", []);
+angular.module("pages.groups", ["services.data", "services.modal"]);
 
 angular.module("app", ["templates", "ngRoute", "ui.bootstrap", "pages"]);
