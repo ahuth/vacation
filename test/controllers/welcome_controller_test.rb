fc42917a5e80e1fddf155b63dbbdcae2af355288
Andrew <andrewhuth2@live.com>
2014-03-20 23:09:56 -0400

    Preload application data

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -5,4 +5,17 @@ class WelcomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test "should get preload when signed in" do
+    sign_in users(:one)
+    get :index
+
+    group = groups(:one)
+    assert_equal assigns(:preload), %Q{{"groups":[{"id":#{group.id},"name":"one"}]}}
+  end
+
+  test "should not have preload when signed out" do
+    get :index
+    assert_not assigns(:preload)
+  end
 end
