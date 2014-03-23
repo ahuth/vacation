51b1430b2d46a2c92a92e8930909f7b4aaee7276
Andrew <andrewhuth2@live.com>
2014-03-23 06:37:31 -0400

    Validate group names

--- app/assets/javascripts/services/data/group.js
+++ app/assets/javascripts/services/data/group.js
@@ -18,6 +18,21 @@ angular.module("services.data").factory("groupData", ["$http", "$q", function ($
   // Internal data cache, alphabetized.
   var data = App.data.groups.sort(comparator);
 
+  // Determine if a name has already been taken.
+  function isUniqueName(name) {
+    return data.every(function (element, index) {
+      return element.name.toLowerCase() !== name.toLowerCase();
+    });
+  }
+
+  // Determine if a group name is valid or not.
+  function isValid(name) {
+    if (name && isUniqueName(name)) {
+      return true;
+    }
+    return false;
+  }
+
   // Return data for all of the groups.
   function all() {
     return data;
@@ -27,6 +42,14 @@ angular.module("services.data").factory("groupData", ["$http", "$q", function ($
   // one, and return a promise indicating if the server action was completed.
   function create(name) {
     var deferred = $q.defer();
+
+    // If the created group will not be valid, reject the promise and exit
+    // early.
+    if (!isValid(name)) {
+      deferred.reject({ errors: ["Invalid name"] });
+      return deferred.promise;
+    }
+
     data.push({ name: name });
 
     $http({
@@ -51,7 +74,13 @@ angular.module("services.data").factory("groupData", ["$http", "$q", function ($
   // Update a group in the client, send a server request to update it, and
   // return a promise indicating if the server action was completed.
   function update(id, options) {
-    var modified = false;
+    var deferred = $q.defer();
+
+    if (!isValid(options.name)) {
+      deferred.reject();
+      return deferred.promise;
+    }
+
     // Find the group we want to edit. Use an `every` loop instead of
     // `forEach` so that we can break out of the loop once we've found the
     // group.
@@ -59,7 +88,6 @@ angular.module("services.data").factory("groupData", ["$http", "$q", function ($
       if (group.id === id) {
         if (options.name && options.name !== group.name) {
           data[index].name = options.name;
-          modified = true;
         }
         return false;
       }
@@ -71,13 +99,17 @@ angular.module("services.data").factory("groupData", ["$http", "$q", function ($
 
     // Make the server request and return a promise, but only if we actually
     // modified a group.
-    if (modified) {
-      return $http({
-        method: "PATCH",
-        url: "/api/groups/" + id,
-        data: { name: options.name }
-      });
-    }
+    $http({
+      method: "PATCH",
+      url: "/api/groups/" + id,
+      data: { name: options.name }
+    }).then(function () {
+      deferred.resolve();
+    }, function (response) {
+      deferred.reject(response);
+    });
+
+    return deferred.promise;
   }
 
   // Delete a group from the client, send a server request to delete it, and
