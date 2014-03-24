825f9f4e0ebf78ae28246fa2ba2ab7270867e355
Andrew <andrewhuth2@live.com>
2014-03-24 14:18:02 -0400

    Sort groups initially in serializer, not on the client

--- app/assets/javascripts/services/data/group.js
+++ app/assets/javascripts/services/data/group.js
@@ -143,9 +143,6 @@ angular.module("services.data").factory("groupData", ["$http", "$q", function ($
     }
   }
 
-  // Sort the initial groups.
-  data.sort(comparator);
-
   return {
     all: all,
     create: create,
--- app/serializers/preload_serializer.rb
+++ app/serializers/preload_serializer.rb
@@ -1,3 +1,6 @@
 class PreloadSerializer < ActiveModel::Serializer
   has_many :groups
+  def groups
+    object.groups.order(:name)
+  end
 end
