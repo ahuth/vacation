144037ec9d3af362f5e26b763fee728e28aeb928
Andrew <andrewhuth2@live.com>
2014-03-25 14:15:30 -0400

    Moved confirm modal controller outside of factory

--- app/assets/javascripts/services/modal/confirm.js
+++ app/assets/javascripts/services/modal/confirm.js
@@ -1,22 +1,23 @@
 /*jslint vars: true, browser: true , nomen: true, indent: 2*/
 /*global angular */
 
+angular.module("services.modal").controller("confirmModalController", ["$scope", "$modalInstance", function ($scope, $modalInstance) {
+  "use strict";
+  $scope.ok = function () {
+    $modalInstance.close(true);
+  };
+  $scope.cancel = function () {
+    $modalInstance.dismiss(false);
+  };
+}]);
+
 angular.module("services.modal").factory("confirmModal", ["$modal", function ($modal) {
   "use strict";
-
-  function confirmController($scope, $modalInstance) {
-    $scope.ok = function () {
-      $modalInstance.close(true);
-    };
-    $scope.cancel = function () {
-      $modalInstance.dismiss(false);
-    };
-  }
 
   function open() {
     return $modal.open({
       templateUrl: "services/modal/confirm.tmpl.html",
-      controller: confirmController
+      controller: "confirmModalController"
     }).result;
   }
 
