a8a6d88f6754e105d9785d41d277a1850128f379
Andrew <andrewhuth2@live.com>
2014-03-25 22:21:56 -0400

    Added employee modal dialogue

---
+++ app/assets/javascripts/services/modal/employee.js
@@ -0,0 +1,42 @@
+/*jslint vars: true, browser: true , nomen: true, indent: 2*/
+/*global angular */
+
+angular.module("services.modal").controller("employeeModalController", ["$scope", "$modalInstance", "title", "name", "hired", function ($scope, $modalInstance, title, name, hired) {
+  "use strict";
+  $scope.data = { name: name, hired: hired };
+  $scope.title = title;
+  $scope.ok = function () {
+    $modalInstance.close($scope.data);
+  };
+  $scope.cancel = function () {
+    $modalInstance.dismiss("cancel");
+  };
+}]);
+
+angular.module("services.modal").factory("employeeModal", ["$modal", "utils", function ($modal, utils) {
+  "use strict";
+
+  function open(options) {
+    var defaults = {
+      title: "Employee",
+      name: "",
+      hired: ""
+    };
+
+    var attributes = utils.merge(defaults, options);
+
+    return $modal.open({
+      templateUrl: "services/modal/employee.tmpl.html",
+      controller: "employeeModalController",
+      resolve: {
+        title: function () { return attributes.title; },
+        name: function () { return attributes.name; },
+        hired: function () { return attributes.hired; }
+      }
+    }).result;
+  }
+
+  return {
+    open: open
+  };
+}]);
