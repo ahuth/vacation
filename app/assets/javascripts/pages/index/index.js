ecc17a33a49564301f4f2a6899c1fb05c668a861
Andrew <andrewhuth2@live.com>
2014-03-25 22:47:32 -0400

    Added employee actions

--- app/assets/javascripts/pages/index/index.js
+++ app/assets/javascripts/pages/index/index.js
@@ -1,7 +1,7 @@
 /*jslint vars: true, browser: true , nomen: true, indent: 2*/
 /*global angular */
 
-angular.module("pages.index").controller("index", ["$scope", "groupData", "groupModal", "confirmModal", function ($scope, groupData, groupModal, confirmModal) {
+angular.module("pages.index").controller("index", ["$scope", "groupData", "employeeData", "groupModal", "employeeModal", "confirmModal", function ($scope, groupData, employeeData, groupModal, employeeModal, confirmModal) {
   "use strict";
   $scope.groups = groupData.all();
   $scope.setGroup = function (group) {
@@ -20,6 +20,26 @@ angular.module("pages.index").controller("index", ["$scope", "groupData", "group
   $scope.removeGroup = function (group) {
     confirmModal.open({ title: "Delete " + group.name + "?"}).then(function () {
       return groupData.destroy(group);
+    });
+  };
+  $scope.newEmployee = function () {
+    employeeModal.open({ title: "New person" }).then(function (attributes) {
+      return employeeData.create($scope.group, attributes);
+    });
+  };
+  $scope.editEmployee = function (employee) {
+    var options = {
+      title: "Edit " + employee.name,
+      name: employee.name,
+      hired: employee.hired
+    };
+    employeeModal.open(options).then(function (attributes) {
+      return employeeData.update(employee, attributes);
+    });
+  };
+  $scope.removeEmployee = function (employee) {
+    confirmModal.open({ title: "Delete " + employee.name + "?" }).then(function () {
+      return employeeData.destroy($scope.group, employee);
     });
   };
 }]);
