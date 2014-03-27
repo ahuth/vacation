b473c76b8c60616655145d9c2c57e02b78c631d5
Andrew <andrewhuth2@live.com>
2014-03-27 14:52:55 -0400

    Split signed-in/out views into separate templates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    # If the user is signed in, render the `show` template with a data preload.
+    # Otherwise, render the `index` template, which is a welcome screen.
     if user_signed_in?
       @preload = PreloadSerializer.new(current_user, root: false).to_json
+      render :show if user_signed_in?
     end
   end
 end
