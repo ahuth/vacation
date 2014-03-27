3f987f2a940a3343fa94aaf845c18db48c08f943
Andrew <andrewhuth2@live.com>
2014-03-27 14:30:58 -0400

    Split group/employee accordion into directive

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,10 @@
 /*jslint vars: true, browser: true , nomen: true, indent: 2*/
 /*global angular */
 
+angular.module("directives.groups-list", ["services.data", "services.modal"]);
+
 angular.module("pages", ["pages.index"]);
-angular.module("pages.index", ["services.data", "services.modal"]);
+angular.module("pages.index", ["directives.groups-list"]);
 
 angular.module("services.data", []);
 angular.module("services.modal", ["services.utils"]);
