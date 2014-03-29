093c74634c5e9349bc2455d33fb22bae67ec48bc
Andrew <andrewhuth2@live.com>
2014-03-29 15:56:54 -0400

    Test changing year in index page

--- app/assets/javascripts/pages/index/controller.js
+++ app/assets/javascripts/pages/index/controller.js
@@ -5,10 +5,18 @@ angular.module("pages.index").controller("indexController", ["$scope", function 
   "use strict";
   $scope.group = "";
   $scope.month = new Date().getMonth() + 1;
+  $scope.year = new Date().getYear();
+
   $scope.prevMonth = function () {
     $scope.month = $scope.month - 1;
   };
   $scope.nextMonth = function () {
     $scope.month = $scope.month + 1;
+  };
+  $scope.prevYear = function () {
+    $scope.year = $scope.year - 1;
+  };
+  $scope.nextYear = function () {
+    $scope.year = $scope.year + 1;
   };
 }]);
