944a42baa0aff02e5003cc14d27e83e7818e5657
Andrew <andrewhuth2@live.com>
2014-03-30 15:27:01 -0400

    Removed test methods from index page

--- app/assets/javascripts/pages/index/controller.js
+++ app/assets/javascripts/pages/index/controller.js
@@ -4,19 +4,4 @@
 angular.module("pages.index").controller("indexController", ["$scope", function ($scope) {
   "use strict";
   $scope.group = "";
-  $scope.month = new Date().getMonth() + 1;
-  $scope.year = new Date().getYear();
-
-  $scope.prevMonth = function () {
-    $scope.month = $scope.month - 1;
-  };
-  $scope.nextMonth = function () {
-    $scope.month = $scope.month + 1;
-  };
-  $scope.prevYear = function () {
-    $scope.year = $scope.year - 1;
-  };
-  $scope.nextYear = function () {
-    $scope.year = $scope.year + 1;
-  };
 }]);
