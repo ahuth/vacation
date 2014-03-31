4de6a5a9391e1cf8f0264111d56eecb3d0e4cf82
Andrew <andrewhuth2@live.com>
2014-03-31 11:28:46 -0400

    Serialize parent ids

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,6 +1,6 @@
 class EmployeeSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :name, :hired
+  attributes :id, :name, :hired, :group_id
   has_many :requests
 end
--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,3 +1,3 @@
 class RequestSerializer < ActiveModel::Serializer
-  attributes :id, :date, :approved
+  attributes :id, :date, :approved, :employee_id
 end
