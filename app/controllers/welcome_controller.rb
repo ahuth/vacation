5138ba5f551ac9492bf74c63218a36299e06859f
Andrew <andrewhuth2@live.com>
2014-03-30 21:56:53 -0400

    Removed unnecessary user_signed_in? check

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
     # Otherwise, render the `index` template, which is a welcome screen.
     if user_signed_in?
       @preload = PreloadSerializer.new(current_user, root: false).to_json
-      render :show if user_signed_in?
+      render :show
     end
   end
 end
