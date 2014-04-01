c1385841b38a591b01b37009b5dfabb4f2670090
Andrew <andrewhuth2@live.com>
2014-04-01 09:44:04 -0400

    Added basic calendarYear directive

---
+++ app/assets/javascripts/directives/calendar_year/controller.js
@@ -0,0 +1,12 @@
+/*jslint vars: true, browser: true , nomen: true, indent: 2*/
+/*global angular */
+
+angular.module("directives.calendarYear").controller("calendarYearController", ["$scope", function ($scope) {
+  "use strict";
+  $scope.year = new Date().getFullYear();
+  $scope.months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (num) {
+    return {
+      month: num
+    };
+  });
+}]);
