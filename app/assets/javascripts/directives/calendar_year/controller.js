74e9259cdb89d36bda837ed902d5bbf80d01afe3
Andrew <andrewhuth2@live.com>
2014-04-01 09:53:11 -0400

    Pass in all requests to calendarYear

--- app/assets/javascripts/directives/calendar_year/controller.js
+++ app/assets/javascripts/directives/calendar_year/controller.js
@@ -1,8 +1,9 @@
 /*jslint vars: true, browser: true , nomen: true, indent: 2*/
 /*global angular */
 
-angular.module("directives.calendarYear").controller("calendarYearController", ["$scope", function ($scope) {
+angular.module("directives.calendarYear").controller("calendarYearController", ["$scope", "requestData", function ($scope, requestData) {
   "use strict";
+  $scope.requests = requestData.all();
   $scope.year = new Date().getFullYear();
   $scope.months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (num) {
     return {
