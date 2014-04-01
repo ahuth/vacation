e6da805e71a5a94cf3cbf4c30acac9865d778e1a
Andrew <andrewhuth2@live.com>
2014-04-01 15:10:22 -0400

    Added <calendar-year> to index page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,7 +7,7 @@ angular.module("directives.employeesList", ["services.data", "services.modal"]);
 angular.module("directives.groupsList", ["services.data", "services.modal"]);
 
 angular.module("pages", ["pages.index"]);
-angular.module("pages.index", ["directives.employeesList", "directives.groupsList"]);
+angular.module("pages.index", ["directives.calendarYear", "directives.employeesList", "directives.groupsList"]);
 
 angular.module("services.data", []);
 angular.module("services.modal", ["services.utils"]);
