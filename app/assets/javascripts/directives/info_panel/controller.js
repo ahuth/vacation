ec27418c232d81fcba07c20755850731d78ca0f7
Andrew <andrewhuth2@live.com>
2014-04-02 11:11:51 -0400

    Make infoPanel scope watching more concise

--- app/assets/javascripts/directives/info_panel/controller.js
+++ app/assets/javascripts/directives/info_panel/controller.js
@@ -4,22 +4,17 @@
 angular.module("directives.infoPanel").controller("infoPanelController", ["$scope", function ($scope) {
   "use strict";
 
+  // When the active group changes, show the group panel. If the group is
+  // deleted, don't show any panel.
   $scope.$watch("group", function (group) {
-    if (!group) {
-      $scope.groupActive = false;
-    } else {
-      $scope.groupActive = true;
-    }
+    $scope.groupActive = !!group;
     $scope.employeeActive = false;
   });
 
+  // When the active employee changes, show the employee panel. If the employee
+  // is deleted, hide the employee panel and show the group one.
   $scope.$watch("employee", function (employee) {
-    if (!employee) {
-      $scope.groupActive = true;
-      $scope.employeeActive = false;
-    } else {
-      $scope.groupActive = false;
-      $scope.employeeActive = true;
-    }
+    $scope.employeeActive = !!employee;
+    $scope.groupActive = !$scope.employeeActive;
   });
 }]);
