99c14ee7885d4c703c48c49587a81b91645c0d64
Andrew <andrewhuth2@live.com>
2014-04-06 07:49:52 -0400

    Sort dates and return when closed in modal

--- app/assets/javascripts/services/modal/request.js
+++ app/assets/javascripts/services/modal/request.js
@@ -3,13 +3,35 @@
 
 angular.module("services.modal").controller("requestModalController", ["$scope", "$modalInstance", "attributes", function ($scope, $modalInstance, attributes) {
   "use strict";
+
+  // Compare two dates to determine their sort order.
+  function dateComparator(a, b) {
+    if (a.isBefore(b)) {
+      return -1;
+    }
+    if (a.isAfter(b)) {
+      return 1;
+    }
+    return 0;
+  }
+
+  // Process an array of dates by sorting it and removing any duplicates.
+  function processDates(dates) {
+    var sorted = dates.sort(dateComparator);
+    return sorted.filter(function (element, index, array) {
+      return array.indexOf(element) === index;
+    });
+  }
+
+  var dates = processDates(attributes.dates);
+
   $scope.title = attributes.title;
-  $scope.days = attributes.days.map(function (day) {
-    return day.date.format("MMMM Do");
+  $scope.days = dates.map(function (date) {
+    return date.format("MMMM Do");
   });
 
   $scope.ok = function () {
-    $modalInstance.close(true);
+    $modalInstance.close(dates);
   };
   $scope.cancel = function () {
     $modalInstance.dismiss(false);
