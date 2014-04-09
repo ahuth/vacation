48b03ab7d3c80f056223f0f0628b2f484d61e051
Andrew <andrewhuth2@live.com>
2014-04-09 07:26:54 -0400

    Add employeeYear directive

---
+++ app/assets/javascripts/directives/employee_year/directive.js
@@ -0,0 +1,15 @@
+/*jslint vars: true, browser: true , nomen: true, indent: 2*/
+/*global angular */
+
+angular.module("directives.employeeYear").directive("employeeYear", function () {
+  "use strict";
+  return {
+    restrict: "AE",
+    templateUrl: "directives/employee_year/template.html",
+    controller: "employeeYearController",
+    scope: {
+      year: "@",
+      employee: "="
+    }
+  };
+});
