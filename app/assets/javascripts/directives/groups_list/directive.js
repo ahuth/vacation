2bd537adc565600058824916903630b31e78d7ec
Andrew <andrewhuth2@live.com>
2014-04-14 17:24:12 -0400

    Add employee attribute to groupsList

--- app/assets/javascripts/directives/groups_list/directive.js
+++ app/assets/javascripts/directives/groups_list/directive.js
@@ -8,7 +8,8 @@ angular.module("directives.groupsList").directive("groupsList", function () {
     templateUrl: "directives/groups_list/template.html",
     controller: "groupsListController",
     scope: {
-      group: "="
+      group: "=",
+      employee: "="
     }
   };
 });
