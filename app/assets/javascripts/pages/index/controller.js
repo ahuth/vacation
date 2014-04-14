6032deda0e68a576ea14cf779654ef2163a1d825
Andrew <andrewhuth2@live.com>
2014-04-14 17:29:51 -0400

    Remove group click listener

--- app/assets/javascripts/pages/index/controller.js
+++ app/assets/javascripts/pages/index/controller.js
@@ -5,12 +5,4 @@ angular.module("pages.index").controller("indexController", ["$scope", function 
   "use strict";
   $scope.group = null;
   $scope.employee = null;
-
-  // Set the active group. We do this with an event instead of 2-way data
-  // binding because the <groups-list> doesn't have access to the `employee`
-  // variable.
-  $scope.$on("group-clicked", function (event, group) {
-    $scope.group = group;
-    $scope.employee = null;
-  });
 }]);
