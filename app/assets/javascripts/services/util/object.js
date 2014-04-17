cc0794f3f6c21b8e03d4fdd26a0a467106a81e96
Andrew <andrewhuth2@live.com>
2014-04-17 10:39:17 -0400

    Split `merge` into its own function

--- app/assets/javascripts/services/util/object.js
+++ app/assets/javascripts/services/util/object.js
@@ -4,8 +4,16 @@
 angular.module("services.utils").factory("objectUtils", function () {
   "use strict";
 
+  // Merge two objects together. The first argument to merge should be the
+  // object that will receive new properties. Any additional arguments will
+  // have their properties merged in to the first.
+  function merge() {
+    var args = Array.prototype.slice.call(arguments);
+    // In jQuery, merge is called `extend`.
+    return $.extend.apply(null, args);
+  }
+
   return {
-    // Merge two objects together. In jQuery, this is called `extend`.
-    merge: $.extend
+    merge: merge
   };
 });
