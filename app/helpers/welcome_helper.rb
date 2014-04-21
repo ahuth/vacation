0a216447f068ce74b42c35514a69cf7823a1d4a2
Andrew <andrewhuth2@live.com>
2014-04-21 16:56:59 -0400

    Add a nav link helper

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
 module WelcomeHelper
+  # Create a anchor nested under a list item. Any options are applied to the
+  # anchor element.
+  def nav_link(link_text, link_path, options={})
+    class_name = current_page?(link_path) ? "active" : nil
+
+    content_tag(:li, class: class_name) do
+      link_to(link_text, link_path, options)
+    end
+  end
 end
--- test/helpers/welcome_helper_test.rb
+++ test/helpers/welcome_helper_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class WelcomeHelperTest < ActionView::TestCase
+  attr_reader :request
+
+  setup do
+    @link_path = "/path"
+    @link_text = "Link"
+  end
+
+  test "should render a list item and link if not on current path" do
+    html = "<li><a href=\"#{@link_path}\">#{@link_text}</a></li>"
+    assert_equal html, nav_link(@link_text, @link_path)
+  end
+
+  test "should add a class if on the link path" do
+    request.path = @link_path
+
+    html = "<li class=\"active\"><a href=\"#{@link_path}\">#{@link_text}</a></li>"
+    assert_equal html, nav_link(@link_text, @link_path)
+  end
 end
