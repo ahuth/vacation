9c7b20dd8f0acfe490fc6351713354fa0425ce9d
Andrew <andrewhuth2@live.com>
2014-04-22 18:14:21 -0400

    Normalize client side config whitespace

--- app/assets/javascripts/config/csrf.js
+++ app/assets/javascripts/config/csrf.js
@@ -3,7 +3,6 @@
 
 angular.module("app").config(["$httpProvider", function ($httpProvider) {
   "use strict";
-
   var csrfToken = $("meta[name=\"csrf-token\"]").attr("content");
   $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken;
 }]);
