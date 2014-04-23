ca4e663eb0a6d0c64bfae321895478c618c550b1
Andrew <andrewhuth2@live.com>
2014-04-23 14:00:36 -0400

    Comment

--- app/assets/javascripts/services/route/route.js
+++ app/assets/javascripts/services/route/route.js
@@ -3,6 +3,9 @@
 
 angular.module("services.route").factory("route", ["$route", "$rootScope", "$location", function ($route, $rootScope, $location) {
   "use strict";
+
+  // Make sure that angular does not reload the view/controller after the next
+  // location change.
   function skipNextReload() {
     var lastRoute = $route.current;
     var unbind = $rootScope.$on("$locationChangeSuccess", function () {
